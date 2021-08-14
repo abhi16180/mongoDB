49f69b9e2fa47ab57930f86143ef430a61f73f56
AdityaSubrahmanyaBhat <70971391+AdityaSubrahmanyaBhat@users.noreply.github.com>
2021-08-14 08:32:34 +0530

    connection is now working

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const User = require("./model/user");
-require("dotenv/config")
+
+require("dotenv").config();
+//TODO: EE import eethara ne iravu bcoz this config function loads the key=value that u enter in the .env file
+//TODO:aa file li key=value format li iravu
+//TODO:This function will then change it to {key:"value"} format and load it to process.env
+//TODO:That is how we get access to the value
 
 app.use(express.json())
 
+ 
 
-
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
     res.send('first req');
 });
 app.get("/users", (req, res) => {
@@ -30,15 +35,22 @@ app.post('/create-user', async (req, res) => {
     }
 
 });
+
 mongoose.connect(process.env.DB_STRING,
     {
         useUnifiedTopology: true,
         useNewUrlParser: true,
     }
-    , (req, res) => {
+    , (err, conn) => {
+        if (err) console.log(`Error in connecting ${err}`)
+        else {
+            console.log(`connected to database ${conn.connection.host}`);
+        }
+    });
+//TODO:Ee function na callback li req an res irtille , bcoz it is not a request
+//TODO:idralli just err and returned connection irtu ashte
 
-        console.log('connected to database');
-    })
+
 app.listen(3000, () => {
     console.log('listening to 3000')
 });
