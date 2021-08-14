576ff234132142ce1b57ae5d33e4774b1fc7cc92
abhi16180 <64468853+abhi16180@users.noreply.github.com>
2021-08-14 14:43:31 +0530

    second commit

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const User = require("./model/user");
-require("dotenv/config")
+require("dotenv/config");
 
-app.use(express.json())
+app.use(express.json()) //for json req
 
 
-
+/*
 app.get('/', (req, res) => {
     res.send('first req');
 });
@@ -24,7 +24,7 @@ app.post('/create-user', async (req, res) => {
     try {
         const myuser = new User(req.body);
         await myuser.save();
-        res.send('user');
+        res.send(`${myuser}`);
     } catch (err) {
         res.send({ 'message': err });
     }
@@ -35,10 +35,36 @@ mongoose.connect(process.env.DB_STRING,
         useUnifiedTopology: true,
         useNewUrlParser: true,
     }
-    , (req, res) => {
+    , (err, conn) => {
 
         console.log('connected to database');
     })
 app.listen(3000, () => {
     console.log('listening to 3000')
+}); */
+app.get( '/',(req,res)=>{
+res.send('get request')
 });
+
+app.get('/display',(req,res)=>{
+    var user=['one','two'];
+    res.send({
+        'user':user
+    })
+})
+mongoose.connect(  
+    process.env.DB_STRING, 
+    {
+        useNewUrlParser:true, 
+        useUnifiedTopology:true, 
+    }, 
+    (err,conn)=>{
+    console.log('DB connected');
+    }
+    
+);
+
+const p=process.env.PORT
+app.listen(p,()=>{
+console.log( `running at ${p}`);
+})
